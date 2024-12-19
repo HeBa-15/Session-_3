86f8089832a860831e9cb2aa8efcf7bcbd7681c6
HeBa-15 <hebamahmoudali1510@gmail.com>
2024-12-19 23:16:42 +0200

    Solve Q1

---
+++ Assigment/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1:1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+
+
+            Console.WriteLine("Enter the Number");
+            int Number = int.Parse(Console.ReadLine());
+
+            if (Number%3==0 && Number % 4 == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+
+
+
+
+            #endregion
+        }
+    }
+}
