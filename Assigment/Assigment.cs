5a41e722f7bba7b07c8a938b3d981a0adef3948a
HeBa-15 <hebamahmoudali1510@gmail.com>
2024-12-20 02:07:10 +0200

    Solve Q3

--- Assigment/Assigment.cs
+++ Assigment/Assigment.cs
@@ -26,24 +26,25 @@ namespace Assigment
 
 
 
+
             #region Q2: Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
 
-            Console.WriteLine("Enter integer Number");
+            //Console.WriteLine("Enter integer Number");
 
-            int Number = int.Parse(Console.ReadLine());
+            //int Number = int.Parse(Console.ReadLine());
 
-            if (Number < 0)
-            {
-                Console.WriteLine("negative");
-            }
-            else if (Number > 0)
-            {
-                Console.WriteLine("Positive");
-            }
-            else
-            {
-                Console.WriteLine("Number is Zero");
-            }
+            //if (Number < 0)
+            //{
+            //    Console.WriteLine("negative");
+            //}
+            //else if (Number > 0)
+            //{
+            //    Console.WriteLine("Positive");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is Zero");
+            //}
 
 
 
@@ -52,9 +53,51 @@ namespace Assigment
 
             #region Q3: Write a program that takes 3 integers from the user then prints the max element and the min element.
 
+            Console.WriteLine("Enter first Number ");
+
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter  second Number ");
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter third Number ");
+
+            int num3 = int.Parse(Console.ReadLine());
+
+
+           int maxNumber = num1;
+            int minNumber = num1;
+
+            if (num2 > maxNumber)
+            {
+                maxNumber = num2;
+            }
+            else if (num2 < minNumber)
+            {
+                minNumber = num2;
+            }
+
+            if (num3 > maxNumber)
+            {
+                maxNumber = num3;
+            }
+            else if (num3 < minNumber)
+            {
+                minNumber = num3;
+            }
+
+            Console.WriteLine( $" Max element = {maxNumber}");
+
+            Console.WriteLine($" Min element = {minNumber}");
+
+
+
+
 
 
             #endregion
+
 
         }
     }
